a6df17e37ba0726cfe5c1bb3a07b1beb75de4676
D-Chamber <timothynnguyen95@gmail.com>
2024-10-13 23:49:57 -0400

    added ability for player to move

--- player.py
+++ player.py
@@ -19,3 +19,22 @@ class Player(circleshape.CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation = self.rotation + PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_r]:
+            self.rotate(-dt)
+        if keys[pygame.K_t]:
+            self.rotate(dt)
+        if keys[pygame.K_c]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
