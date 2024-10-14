fcc9190aeb1d827c1690fa91c8ddce027350c875
D-Chamber <timothynnguyen95@gmail.com>
2024-10-14 00:47:44 -0400

    added collision physics and exiting program

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidField import AsteroidField
 
 
 def main():
@@ -15,6 +18,11 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
 
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -25,6 +33,11 @@ def main():
                 return
         for obj in updatable:
             obj.update(dt)
+
+        for obj in asteroids:
+            if obj.check_collided(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
         for obj in drawable:
